d589ab97bd5a9d24946cca0ed0474580c910dad8
sean <31488sean@gmail.com>
2025-09-02 18:02:45 +0800

    efef

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'MyLib',
-      formats: ['es'],
+      // formats: ['es'],
     },
     rollupOptions: {
       external: ['vue'],
