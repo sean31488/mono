60ab5d0428822ee291195d478b5f1d12a908e6b2
sean <31488sean@gmail.com>
2026-01-21 16:50:31 +0800

    bbb

--- packages/site-1/src/main.js
+++ packages/site-1/src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import SeanUIPlugin from '@sean/ui/global'
+import './style.css'
 
-const app = createApp(App)
-app.use(SeanUIPlugin)
-app.mount('#app')
+createApp(App).mount('#app')
