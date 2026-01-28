ec948c7ade26068a7367d82af4fdb33aef94b7cf
Sean Chang <schang@health2sync.com>
2026-01-28 17:34:03 +0800

    branch-1 update 2

--- packages/site-1/src/main.js
+++ packages/site-1/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
+import SeanUIPlugin from '@sean/ui/global'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(SeanUIPlugin)
+app.mount('#app')
